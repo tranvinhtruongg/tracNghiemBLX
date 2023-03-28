8f0b24cfe78b01be7987fa0f47d5c719fe6cffdf
tranvinhtruongg <101718796+tranvinhtruongg@users.noreply.github.com>
2023-03-28 18:40:07 +0700

    layout done

--- app/src/main/java/vn/edu/tdmu/tranvinhtruong/tracnghiemblx/HomeFragment.java
+++ app/src/main/java/vn/edu/tdmu/tranvinhtruong/tracnghiemblx/HomeFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
@@ -14,57 +13,37 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import me.relex.circleindicator.CircleIndicator;
-import vn.edu.tdmu.tranvinhtruong.tracnghiemblx.Question.DBHelper;
-import vn.edu.tdmu.tranvinhtruong.tracnghiemblx.slide.ScreenSlidePagerActivity;
 
-//public class HomeFragment extends Fragment {
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_home, container, false);
-//    }
-//}
-
-public class HomeFragment extends AppCompatActivity{
+public class HomeFragment extends Fragment {
     private ViewPager viewPager;
     private CircleIndicator circleIndicator;
     private PhotoAdapter photoAdapter;
     Button btnThi, btnHoc, btnBienBao;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_home);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        viewPager=findViewById(R.id.viewpager);
-        circleIndicator=findViewById(R.id.circle_indicator);
 
-        photoAdapter=new PhotoAdapter(this,getListPhoto());
+        viewPager=view.findViewById(R.id.viewpager);
+        circleIndicator=view.findViewById(R.id.circle_indicator);
+
+        photoAdapter = new PhotoAdapter(getContext(),getListPhoto());
         viewPager.setAdapter(photoAdapter);
 
         circleIndicator.setViewPager(viewPager);
         photoAdapter.registerDataSetObserver(circleIndicator.getDataSetObserver());
 
-        btnThi = (Button) findViewById(R.id.btnThi);
-        btnThi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(HomeFragment.this, ScreenSlidePagerActivity.class);
-                startActivity(intent);
-            }
-        });
-        DBHelper db =new DBHelper(getApplicationContext());
-        try {
-            db.createDataBase();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
     private List<Photo> getListPhoto(){
         List<Photo> list= new ArrayList<>();
--- app/src/main/java/vn/edu/tdmu/tranvinhtruong/tracnghiemblx/MainActivity.java
+++ app/src/main/java/vn/edu/tdmu/tranvinhtruong/tracnghiemblx/MainActivity.java
@@ -1,12 +1,12 @@
 package vn.edu.tdmu.tranvinhtruong.tracnghiemblx;
 
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-
 
 import android.os.Bundle;
 import android.view.MenuItem;
